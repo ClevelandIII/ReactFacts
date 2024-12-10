0ad50c6cf300df299c511d82259e122ce5cd71d2
ClevelandIII <kingslime108@gmail.com>
2024-12-09 20:01:37 -0700

    SVG update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@
 export default function Navbar() {
     return (
         <nav>
-            <img src="src/assets/react.svg" alt="" />
+            <img src="./src/assets/react.svg" alt="" />
             <h1>ReactFacts</h1>
         </nav>
     );
